1cef8e4d64b59c281d3d39ed9a44e70a15e6ff5b
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-12 08:55:41 -0700

    changed the state to the pokedex rather than the app component

--- src/App.js
+++ src/App.js
@@ -3,24 +3,10 @@ import "./styles/App.css";
 import { useEffect, useState } from "react";
 
 const App = () => {
-	const [pokedex, setPokedex] = useState([]);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			for (let i = 1; i <= 151; i++) {
-				const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-				const response = await fetch(url);
-				const pokemon = await response.json();
-				setPokedex((pokedex) => pokedex.concat(pokemon));
-			}
-		};
-		fetchData();
-	}, []);
-
 	return (
 		<div className="container">
 			<h1 className="title">Pokedex</h1>
-			<Pokedex pokedex={pokedex} />
+			<Pokedex />
 		</div>
 	);
 };
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/component_styles/Pokedex.css";
 import { Pokemon } from "./Pokemon";
 
-export const Pokedex = ({ pokedex }) => {
+export const Pokedex = () => {
+	const [pokedex, setPokedex] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			for (let i = 1; i <= 151; i++) {
+				const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+				const response = await fetch(url);
+				const pokemon = await response.json();
+				setPokedex((pokedex) => pokedex.concat(pokemon));
+			}
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<div className="pokedex">
 			{pokedex.map((pokemon) => (
