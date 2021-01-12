7bda226a4eb2593f3c1ab76fd69ba700316430e1
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-11 22:29:01 -0700

    added basic styling for pokemon cards

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,5 +2,16 @@ import React from "react";
 import "../styles/component_styles/Pokemon.css";
 
 export const Pokemon = ({ pokemon }) => {
-	return <div className="pokemon">{pokemon.name}</div>;
+	return (
+		<div className="pokemon">
+			<div className="pokemon__card">
+				<img
+					src={pokemon.sprites.front_default}
+					className="pokemon__sprite"
+					alt={pokemon.name}
+				/>
+				<div className="pokemon__name">{pokemon.name}</div>
+			</div>
+		</div>
+	);
 };
