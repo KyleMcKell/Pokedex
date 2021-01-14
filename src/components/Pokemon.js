8d6a2ab7b49794e5d1415e3828fb78f6ed749762
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-14 16:43:22 -0700

    added event listeners to add an active class when hovered

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,7 +2,10 @@ import React, { useEffect } from "react";
 import "../styles/component_styles/Pokemon.css";
 
 export const Pokemon = ({ name, types, id, sprite }) => {
+	// get the pokemon from the document
+
 	useEffect(() => {
+		// make an array of the types passed from props
 		const typeArr = [];
 		for (const type of types) {
 			if (type.type.name != null) {
@@ -10,11 +13,19 @@ export const Pokemon = ({ name, types, id, sprite }) => {
 			}
 		}
 		const pokemon = document.querySelector(`.pokemon--${id}`);
+		// set a gradient background for the types of the pokemon
 		pokemon.style.backgroundImage =
 			typeArr.length > 1
 				? `linear-gradient(to right, var(--${typeArr[0]}1), var(--${typeArr[1]}1)`
 				: `linear-gradient(var(--${typeArr[0]}1), var(--${typeArr[0]}1)`;
-	});
+
+		pokemon.addEventListener("mouseover", () => {
+			pokemon.classList.add("pokemon__card--active");
+		});
+		pokemon.addEventListener("mouseout", () => {
+			pokemon.classList.remove("pokemon__card--active");
+		});
+	}, [id, types]);
 
 	return (
 		<div className={`pokemon`}>
