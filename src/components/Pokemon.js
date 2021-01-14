15466018ea1a907c2ab27f87e087453d242b7f1b
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-14 14:42:34 -0700

    changed the active to a hover effect

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,26 +5,24 @@ import "../styles/component_styles/Pokemon.css";
 export const Pokemon = ({ pokemon }) => {
 	return (
 		<div className="pokemon">
-			<a
+			{/* <a
 				href={`https://bulbapedia.bulbagarden.net/wiki/${pokemon.name}_(Pok%C3%A9mon)`}
 				className="pokemon__link"
 				target="_blank"
-			>
-				<div
-					className={`pokemon__card pokemon--${pokemon.types["0"].type.name}`}
-				>
-					<img
-						src={pokemon.sprites.front_default}
-						className={`pokemon__sprite`}
-						alt={pokemon.name}
-					/>
-					<div className="pokemon__data">
-						<div className="pokemon__id">{`${pokemon.id}`}</div>
-						<hr class="pokemon__hr" />
-						<div className="pokemon__name">{`${pokemon.name}`}</div>
-					</div>
+			> */}
+			<div className={`pokemon__card pokemon--${pokemon.types["0"].type.name}`}>
+				<img
+					src={pokemon.sprites.front_default}
+					className={`pokemon__sprite`}
+					alt={pokemon.name}
+				/>
+				<div className="pokemon__data">
+					<div className="pokemon__id">{`${pokemon.id}`}</div>
+					<hr class="pokemon__hr" />
+					<div className="pokemon__name">{`${pokemon.name}`}</div>
 				</div>
-			</a>
+			</div>
+			{/* </a> */}
 		</div>
 	);
 };
