614e42f01bd9252e155909f24da0aa79f9fa6113
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-14 13:21:26 -0700

    made it so the entire card is a link

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,22 +5,24 @@ import "../styles/component_styles/Pokemon.css";
 export const Pokemon = ({ pokemon }) => {
 	return (
 		<div className="pokemon">
-			<div className={`pokemon__card pokemon--${pokemon.types["0"].type.name}`}>
-				<img
-					src={pokemon.sprites.front_default}
-					className={`pokemon__sprite`}
-					alt={pokemon.name}
-				/>
-				<div className="pokemon__name">
-					<a
-						href={`https://bulbapedia.bulbagarden.net/wiki/${pokemon.name}_(Pok%C3%A9mon)`}
-						className="pokemon__link"
-						target="_blank"
-					>
+			<a
+				href={`https://bulbapedia.bulbagarden.net/wiki/${pokemon.name}_(Pok%C3%A9mon)`}
+				className="pokemon__link"
+				target="_blank"
+			>
+				<div
+					className={`pokemon__card pokemon--${pokemon.types["0"].type.name}`}
+				>
+					<img
+						src={pokemon.sprites.front_default}
+						className={`pokemon__sprite`}
+						alt={pokemon.name}
+					/>
+					<div className="pokemon__name">
 						{`${pokemon.id}: ${pokemon.name}`}
-					</a>
+					</div>
 				</div>
-			</div>
+			</a>
 		</div>
 	);
 };
