189739dc71358e1d799b9c75d01d0942f4914175
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-13 21:32:12 -0700

    put fetchdata into useeffect, as the plan is to load all pokemon together with a promise all, rather than loading more per click

--- src/App.js
+++ src/App.js
@@ -5,41 +5,36 @@ import { LoadMore } from "./components/LoadMore";
 
 const App = () => {
 	const [pokedex, setPokedex] = useState([]);
-	const [pokemonPerPage] = useState(25);
+	const [maxPokemon] = useState(25);
 	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
-		fetchData(1, pokemonPerPage);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	const fetchData = async (firstPokemonToLoad, amountOfPokemonOnPage) => {
-		setIsLoading(true);
-		for (let i = firstPokemonToLoad; i <= amountOfPokemonOnPage; i++) {
-			const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-			const response = await fetch(url);
-			const pokemon = await response.json();
-			if (pokedex.indexOf(pokemon) === -1) {
-				setPokedex((pokemons) => pokemons.concat(pokemon));
+		const fetchData = async () => {
+			setIsLoading(true);
+			for (let i = 1; i <= maxPokemon; i++) {
+				const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+				const response = await fetch(url);
+				const pokemon = await response.json();
+				if (pokedex.indexOf(pokemon) === -1) {
+					setPokedex((pokemons) => pokemons.concat(pokemon));
+				}
 			}
-		}
-		setIsLoading(false);
-	};
+			setIsLoading(false);
+		};
+		fetchData();
+	}, [maxPokemon]);
 
-	const loadMorePokemon = () => {
-		fetchData(
-			pokemonPerPage - pokemonPerPage + 1 + pokedex.length,
-			pokedex.length + pokemonPerPage
-		);
-	};
+	// const loadMorePokemon = () => {
+	// 	fetchData();
+	// };
 
 	return (
 		<div className="container">
 			<h1 className="title">Pokedex</h1>
 			<Pokedex pokedex={pokedex} />
 			<LoadMore
-				loadMoreOnClick={loadMorePokemon}
-				pokemonToLoad={pokemonPerPage}
+				// loadMoreOnClick={loadMorePokemon}
+				pokemonToLoad={maxPokemon}
 				isLoading={isLoading}
 			/>
 		</div>
