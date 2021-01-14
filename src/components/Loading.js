d77056a1665e704d64eb260dc8fe41f3c78cde00
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-13 22:31:07 -0700

    focused down code to only necessary components, got rid of unecessary fields

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/component_styles/Loading.css";
 
-export const Loading = () => {
-	return (
-		<div className="loading">
-			<h2 className="loading__loadingText">Fetching Pokemon Data</h2>
-		</div>
-	);
+export const Loading = ({ isLoading }) => {
+	if (isLoading) {
+		return (
+			<div className="loading">
+				<h2 className="loading__loadingText">Fetching Pokemon Data</h2>
+			</div>
+		);
+	} else {
+		return <></>;
+	}
 };
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -2,10 +2,6 @@ import "../styles/component_styles/Pokedex.css";
 import { Pokemon } from "./Pokemon";
 
 export const Pokedex = ({ pokedex }) => {
-	// if (loading) {
-	// 	return <h2 className="pokedex__loadingText">Fetching Pokemon Data</h2>;
-	// }
-
 	return (
 		<div className="pokedex">
 			{pokedex.map((pokemon) => (
