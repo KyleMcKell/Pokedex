e881e8c743595ab064668fd5252d124696c3c322
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-14 17:05:05 -0700

    added comments to app.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,34 @@ import { useEffect, useState } from "react";
 import { Loading } from "./components/Loading";
 
 const App = () => {
+	// holds pokemon data
 	const [pokedex, setPokedex] = useState([]);
+	// if true, instead of pokedex showing, shows a loading message
 	const [isLoading, setIsLoading] = useState(false);
 
+	// amount of pokemon that will be loaded in
+	// total created pokemon is 898, 151 is the Kanto region, which is typically what I end up fetching
 	const POKEMON_IN_DEX = 151;
 
+	// fetches pokemon from api
 	useEffect(() => {
 		const fetchData = async () => {
+			// sets the loading screen
 			setIsLoading(true);
+			// makes POKEMON_IN_DEX length array filled with urls that have each pokemon associated
 			const pokeArr = new Array(POKEMON_IN_DEX).fill("").map((_, index) => {
 				return `https://pokeapi.co/api/v2/pokemon/${index + 1}`;
 			});
 			try {
-				const resolvedFetch = await Promise.all(
-					pokeArr.map((url) => fetch(url))
+				// fetches a response for each pokemon url
+				const response = await Promise.all(pokeArr.map((url) => fetch(url)));
+				// changes all responses to json
+				const data = await Promise.all(
+					response.map((pokemonRes) => pokemonRes.json())
 				);
-				const response = await Promise.all(
-					resolvedFetch.map((pokemonRes) => pokemonRes.json())
-				);
-				setPokedex((pokedex) => pokedex.concat(response));
+				// sets the pokedex to the data
+				setPokedex((pokedex) => pokedex.concat(data));
+				// removes loading screen
 				setIsLoading(false);
 			} catch (err) {
 				console.log(err);
