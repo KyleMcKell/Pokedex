652097d711b85295fd138650fb6854972082ff51
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-14 17:06:59 -0700

    added comments to Pokedex.js

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,16 +1,17 @@
 import "../styles/component_styles/Pokedex.css";
 import { Pokemon } from "./Pokemon";
 
+// maps an array of pokemon
 export const Pokedex = ({ pokedex }) => {
 	return (
 		<div className="pokedex">
 			{pokedex.map((pokemon) => (
 				<Pokemon
-					id={pokemon.id}
-					key={pokemon.id}
-					types={pokemon.types}
-					name={pokemon.name}
-					sprite={pokemon.sprites.front_default}
+					id={pokemon.id} // pokemon's dex number
+					key={pokemon.id} // key for React, each dex number is unique
+					types={pokemon.types} // pokemon's types, an object
+					name={pokemon.name} // name of the pokemon
+					sprite={pokemon.sprites.front_default} // image of pokemon
 				/>
 			))}
 		</div>
